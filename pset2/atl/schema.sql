e508c78ed9f36405264ec162e6d340e7c721603a
aayush3692 <140303956+aayush3692@users.noreply.github.com>
2024-07-29 19:42:29 +0545

    schema.sql

---
+++ pset2/atl/schema.sql
@@ -0,0 +1,5 @@
+/*Hartsfield-Jackson International Airport, perhaps better known as “ATL” for its IATA airport code, has been the busiest airport in the world since 1998. 
+Located in Atlanta, Georgia in the United States, ATL served 93.7 million passengers in 2022. Maybe you were one of them!
+
+Suppose you’ve just been hired to help ATL re-design their database system. In a file called schema.sql in a folder called atl,
+write a set of SQL statements to design a database with which Hartsfield-Jackson could keep track of its passengers and their flights.*/
