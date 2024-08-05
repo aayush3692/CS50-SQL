f47d1f226207b66c09c5c00399d78cc12092821a
aayush3692 <140303956+aayush3692@users.noreply.github.com>
2024-08-05 18:15:37 +0545

    Hack.java

---
+++ pset6/Don't Panic! In Java/Hack.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Hack {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the new password: ");
+        String password = scanner.nextLine();
+
+
+       String query = "UPDATE \"users\" SET \"password\" = '" + password + "' WHERE \"username\" = 'admin';";
+
+       Connection sqliteConnection = DriverManager.getConnection("jdbc:sqlite:dont-panic.db");
+
+        Statement sqliteStatement = sqliteConnection.createStatement();
+
+        sqliteStatement.executeUpdate(query);
+
+        sqliteConnection.close();
+        scanner.close();
+    }
+}
